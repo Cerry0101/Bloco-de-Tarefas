b5c6166a2294996a56667956167cb337f461e478
Marcelle Lima <cerry0101@gmail.com>
2022-04-13 15:51:30 -0300

    entendendo o state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react' //importar o react, para que ele reconheça as funções do React.
+import React, { useState } from 'react' //importar o react, para que ele reconheça as funções do React.
+import "./App.css"; //importar o css para utilizar as suas classes 
 
 const App = () => { //criar uma função
-     const falabro = "hello world" ; //usando js, basta por as variaveis entre {}
-    
-    return <h1>{falabro}</h1>;
+     //const falabro = "hello world" ; //usando js, basta por as variaveis entre {<h1>{falabro}</h1>}
+     // let falabro = "hello world" ; //As variáveis de let podem ser atualizadas, mas não podem ser declaradas novamente. As variáveis de const não podem ser atualizadas nem declaradas novamente. Todas elas passam por hoisting para o topo de seu escopo.
+     const [falabro, setFalabro] = useState('oi bro') //State (estado do componente), É como uma variável que guardamos um valor, porém quando ele é atualizado o componente é renderizado novamente.
+
+    return (
+        <div> 
+            {
+            /*precisa renderizar tudo do componente em uma div 
+            somente ou dentro de um fragment <> </>*/
+            }
+
+            <div className="container">{falabro}</div> {/*é um container genérico para conteúdo de fluxo, que de certa forma não representa nada. */}
+            <button onClick={() => setFalabro("hellooooooooo")}>
+            {/* console.log("ois") forma de debugar códigos*/}
+            mudar mensagem
+            </button>
+        </div>
+    );
 };
 
 export default App; //exportar o App, para que o index importe.
